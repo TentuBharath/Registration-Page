330ca614ba3e4aa780ecdaca396283e87738beca
Tentu Bharath <91159716+TentuBharath@users.noreply.github.com>
2023-07-11 17:47:12 +0530

    Add files via upload

---
+++ Registrationpage.js
@@ -0,0 +1,8 @@
+let inputElement = document.getElementById("inputElement");
+let result = document.getElementById("result");
+function getRegister() {
+  let inputValue = inputElement.inputValue;
+  alert(
+    (verifyText = "Hii " + inputValue + ", You are successfully register...")
+  );
+}
